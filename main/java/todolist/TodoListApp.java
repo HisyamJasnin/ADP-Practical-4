a843102374e18413d145e78072e9e054aac5eff4
Sam <19FTT1067@student.edu.bn>
2021-04-25 21:23:04 +0800

    Lesson 1.3 - View component (Login.fxml)

--- main/java/todolist/TodoListApp.java
+++ main/java/todolist/TodoListApp.java
@@ -1,11 +1,20 @@
 package todolist;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class TodoListApp extends Application {
 	
 	public void start(Stage arg0) throws Exception{
+		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("view/Login.fxml"));
+		Parent root = fxmlLoader.load();
+				
+		Scene scene = new Scene(root, 500, 500);
+		arg0.setScene(scene);
+		
 		arg0.setTitle("Todo List App");
 		arg0.setResizable(false);
 		arg0.sizeToScene();
